8d2fb380bc038b64d1b8e3515fefe30e216b1b6d
lKanade <Kanade@outlook.es>
2017-07-28 09:33:52 -0600

    Agragado metodo para max y min, no se en la interfaz donde la piensan poner

---
+++ Capa_Negocios/CargarMaxMin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Capa_Negocios {
+    public class CargarMaxMin {
+
+        public DataTable registroMayor(string strDataBase,string strColumna,string strTabla , String strInstancia , String var = "master") {
+            Capa_Conexion.MaxMin clMaxMin = new Capa_Conexion.MaxMin ();
+            return clMaxMin.cargarMax (strInstancia , strDataBase , strTabla , strColumna);
+        }
+
+        public DataTable registroMenor(string strDataBase , string strColumna , string strTabla , String strInstancia , String var = "master") {
+            Capa_Conexion.MaxMin clMaxMin = new Capa_Conexion.MaxMin ();
+            return clMaxMin.cargarMin (strInstancia , strDataBase , strTabla , strColumna);
+        }
+    }
+}
