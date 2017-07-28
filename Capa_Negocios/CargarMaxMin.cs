29b75aa3b7318fc2766a9bf5822bfdf0d8b98d7d
lKanade <Kanade@outlook.es>
2017-07-28 09:47:48 -0600

    No se si usar listas para saber la fila con el dato o no es necesario

--- Capa_Negocios/CargarMaxMin.cs
+++ Capa_Negocios/CargarMaxMin.cs
@@ -8,13 +8,21 @@ using System.Data;
 namespace Capa_Negocios {
     public class CargarMaxMin {
 
-        public DataTable registroMayor(string strDataBase,string strColumna,string strTabla , String strInstancia , String var = "master") {
+        // NO SE SI USAR LISTAS PARA OBTENER LA FILA QUE CONTIENE EL VALOR O CON SOLO EL VALOR BASTA
+
+        List<string> listRegistro = new List<string> ();
+
+        public DataTable registroMayor(string strDataBase,string strColumna,string strTabla , String strInstancia) {
             Capa_Conexion.MaxMin clMaxMin = new Capa_Conexion.MaxMin ();
+            listRegistro = new List<string>();
+
             return clMaxMin.cargarMax (strInstancia , strDataBase , strTabla , strColumna);
         }
 
-        public DataTable registroMenor(string strDataBase , string strColumna , string strTabla , String strInstancia , String var = "master") {
+        public DataTable registroMenor(string strDataBase , string strColumna , string strTabla , String strInstancia) {
             Capa_Conexion.MaxMin clMaxMin = new Capa_Conexion.MaxMin ();
+            listRegistro = new List<string> ();
+
             return clMaxMin.cargarMin (strInstancia , strDataBase , strTabla , strColumna);
         }
     }
