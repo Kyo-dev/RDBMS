4e3ef7d9259747917bc398cbce6aa4e5305859f2
mariana9827 <nanaga98@hotmail.com>
2017-07-31 22:56:33 -0600

    Método Byte

--- Capa_Negocios/DataTypeColumns.cs
+++ Capa_Negocios/DataTypeColumns.cs
@@ -7,9 +7,9 @@ using System.Data;
 using System.Text.RegularExpressions;
 
 namespace Capa_Negocios {
-    class DataTypeColumns {
+    public class DataTypeColumns {
         #region Datatype Date
-        public static bool canBeDate(DataColumn dC) {
+        public bool canBeDate(DataColumn dC) {
             bool cantBe = true;
             foreach(DataRow row in dC.Table.Rows) {
                 if(!Regex.IsMatch((String)row[dC], @"(?<!\d)(?:(?:(?:1[6-9]|[2-9]\d)?\d{2})(?:(?:(?:0[13578]|1[02])31)|(?:(?:0[1,3-9]|1[0-2])(?:29|30)))|(?:(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00)))0229)|(?:(?:1[6-9]|[2-9]\d)?\d{2})(?:(?:0?[1-9])|(?:1[0-2]))(?:0?[1-9]|1\d|2[0-8]))(?!\d)")) {//Obviamente expresion regular de internet :v
@@ -37,7 +37,7 @@ namespace Capa_Negocios {
         #endregion
 
         #region Datatype INT
-        public static bool canBeInt(DataColumn dC) {
+        public bool canBeInt(DataColumn dC) {
             bool cantBe = true;
             foreach(DataRow row in dC.Table.Rows) { //Recorre cada registro.
 
@@ -50,6 +50,48 @@ namespace Capa_Negocios {
 
                 Int64 num = Convert.ToInt64(row[dC]); //Necesito convertirlo a dato numero para la comprobar, se cambia al dato entero mas grande
                 if((num < -2147483648 || num > 2147483648) && isNumber) { //Si el dato es completamente numerico verifico el tamaño INT (Ver tabla de tipos de datos en la pagina de Microsoft)
+                    cantBe = false;
+                }
+            }
+            return cantBe;
+        }
+        #endregion
+
+        #region DataType Byte
+        public bool canBeByte (DataColumn dC)
+        {
+            bool cantBe = true;
+            foreach (DataRow row in dC.Table.Rows)
+            { //Recorre cada registro.
+
+                if (!row.Equals(1)|| !row.Equals(0))
+                { 
+                    cantBe = false;
+                }
+            }
+            return cantBe;
+        }
+        #endregion
+
+        #region DataType TinyInt
+        public bool canBeTinyInt (DataColumn dC)
+        {
+            bool cantBe = true;
+            foreach (DataRow row in dC.Table.Rows)
+            { //Recorre cada registro.
+
+                bool isNumber = true;
+                foreach (char c in (String)row[dC])
+                { //Valida que cada char sea numerico
+                    if (!Char.IsNumber(c))
+                    {
+                        isNumber = false;
+                    }
+                }
+
+                Int64 num = Convert.ToInt64(row[dC]); //Necesito convertirlo a dato numero para la comprobar, se cambia al dato entero mas grande
+                if ((num < -2147483648 || num > 2147483648) && isNumber)
+                { //Si el dato es completamente numerico verifico el tamaño INT (Ver tabla de tipos de datos en la pagina de Microsoft)
                     cantBe = false;
                 }
             }
